59309fe5d9f91817d810e6c51d75c53bfc75c09e
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-04-26 14:48:53 +0600

    script

---
+++ 01.Get_Element_selector_Part/script.js
@@ -0,0 +1,11 @@
+let title = document.getElementById('title')
+console.log(title)
+
+let paragraphs = document.getElementsByClassName('lead')
+console.log(paragraphs)
+
+let list = document.getElementsByTagName('li')
+console.log(list)
+
+let listitemone = document.getElementsByName('list-item-one')
+console.log(listitemone)
