8e8271df7ecb544a39e01e5f4f0c6002f0bdf52f
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-05-03 10:56:47 +0600

    js.js

---
+++ 02.Query_selector_part/script.js
@@ -0,0 +1,26 @@
+// let title = document.getElementById('title')
+// console.log(title)
+//
+// let paragraphs = document.getElementsByClassName('lead')
+// console.log(paragraphs)
+//
+// let list = document.getElementsByTagName('li')
+// console.log(list)
+//
+// let listitemone = document.getElementsByName('list-item-one')
+// console.log(listitemone)
+
+let title = document.querySelector('#title')
+console.log(title)
+
+let paragraphs = document.querySelector('.lead')
+console.log(paragraphs)
+
+let paragraphsAll = document.querySelectorAll('.lead')
+console.log(paragraphsAll)
+
+let list = document.querySelectorAll('li')
+console.log(list)
+
+let listitemone = document.querySelector('[name="list-item-one"]')
+console.log(listitemone)
